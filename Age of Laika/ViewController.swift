132b738d1a0f6948149c4aa8163e7df5496441c3
kuchta300 <kuchta.300@icloud.com>
2014-09-27 13:18:31 -0400

    Final of Dog Years Tutorial
    Exercise 3 from IOS Programming

--- Age of Laika/ViewController.swift
+++ Age of Laika/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var humanYearsLabel: UITextField!
+    
+    @IBOutlet weak var dogYearsLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ConvertButtonLabel(sender: UIButton) {
+        
+        let conversionConstant = 7
+        let humanYearsInString  = humanYearsLabel.text
+        let humanYearsInInt = humanYearsInString.toInt()
+        let dogYearsInInt = humanYearsInInt! * 7
+        
+        dogYearsLabel.text = "\(dogYearsInInt)" + " Dog Years"
+        
+        
+    }
 
 }
 
